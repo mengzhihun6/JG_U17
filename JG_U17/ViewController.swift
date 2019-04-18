03b2ceb077f53f6e03d069f81837fe28c12d2406
JG-GuoJun <442647068@qq.com>
2019-04-18 09:30:19 +0800

    Initial Commit

---
+++ JG_U17/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JG_U17
+//
+//  Created by 郭军 on 2019/4/18.
+//  Copyright © 2019 guojun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JG_U17Tests/JG_U17Tests.swift
@@ -0,0 +1,34 @@
+//
+//  JG_U17Tests.swift
+//  JG_U17Tests
+//
+//  Created by 郭军 on 2019/4/18.
+//  Copyright © 2019 guojun. All rights reserved.
+//
+
+import XCTest
+@testable import JG_U17
+
+class JG_U17Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
